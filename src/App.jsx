a4b7c04dd2ae0288a690b926e6d60390413b4a33
Samuel Condack <Samuelcondackxz@gmail.com>
2023-08-30 14:30:59 -0300

    feat: Finish Responsive Home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
 import styles from "./app.module.css"
 import Header from "./components/Header/Header"
-
+import arrow from "./images/arrow.gif"
+ 
 function App() {
   return (
     <>
       <Header />
-      <main>
-
-      </main>
+      <div className={styles.introduction}>
+          <div className={styles.home}>
+            <h1 className={styles.title}>
+              HEY, I'M
+              <p className={styles.nameTitle}>
+                SAMUEL CONDACK
+              </p>
+              </h1>
+            <p className={styles.subTitle}>
+              A Front-end developer building web solutions<br>
+              </br>Passionate about technology and the development world
+            </p>
+            <img style={{pointerEvents: "none"}} className={styles.arrow} src={arrow} alt="arrow down gif"/>
+          </div>
+      </div>
     </>
   )
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,22 +1,44 @@
 import styles from './Header.module.css'
+import menu from '../../images/ham-menu.svg'
+import closeMenu from '../../images/ham-menu-close.svg'
 
+function toggleDisplay(){
+    let open = document.getElementById("openMenu")
+    let close = document.getElementById("closeMenu")
+    let mobile = document.getElementById("mobileMenu")
+    if(open.style.display == "none"){
+        open.style.display = "flex"
+        close.style.display = "none"
+        mobile.style.display = "none"
+    } else if(close.style.display == "none"){
+        close.style.display = "flex"
+        open.style.display = "none"
+        mobile.style.display = "flex"
+    }
+}
 
 export default function Header(){
     return(
         <header>
-         <a href="#" className={styles.iconA}>
+         <a href="#" className={styles.iconLink}>
              <div className={styles.icon}>
              </div>
          </a>
-        <ul className={styles.headerLinks}>
-            <li>
-                <a href="#" className={styles.links + styles.homeIconLink}>
-                    <div className={styles.homeIcon}></div>
-                </a>
-            </li>
-            <li><a href="#" className={styles.links}>About</a></li>
-            <li><a href="#" className={styles.links}>Projects</a></li>
-            <li><a href="#" className={styles.links}>Contact</a></li>
+         <div onClick={toggleDisplay}className={styles.menu}>
+            <img id='openMenu' src={menu} alt="openMenu" />
+            <img style={{display: "none"}} id='closeMenu' src={closeMenu} alt="closeMenu" />
+         </div>
+         <ul id='mobileMenu' className={styles.mobileMenu} >
+            <li><a href="#" className={styles.menuLinks}>Home</a></li>
+            <li><a href="#" className={styles.menuLinks}>About</a></li>
+            <li><a href="#" className={styles.menuLinks}>Projects</a></li>
+            <li><a href="#" className={styles.menuLinks}>Contact</a></li>
+        </ul>
+        <ul className={styles.headerUl}>
+            <li><a href="#" className={styles.headerLinks}>Home</a></li>
+            <li><a href="#" className={styles.headerLinks}>About</a></li>
+            <li><a href="#" className={styles.headerLinks}>Projects</a></li>
+            <li><a href="#" className={styles.headerLinks}>Contact</a></li>
       </ul>
     </header>
     )
