92405139048fab1d909a52709ef74b6d04a0fa0b
Samuel Condack <Samuelcondackxz@gmail.com>
2023-09-08 20:47:52 -0300

    feat: Add Login&Signup Project

--- src/Pages/Projects/Projects.jsx
+++ src/Pages/Projects/Projects.jsx
@@ -1,5 +1,6 @@
 import styles from './Projects.module.css'
 import Project1 from '../../Images/Project1.png'
+import Project2 from '../../Images/Project2.png'
 
 export default function Projects(){
     return(
@@ -16,7 +17,7 @@ export default function Projects(){
                         </div>
                         <div className={styles.projectsRowContent}>
                             <h3 className={styles.projectsRowContentTitle}>Portfolio</h3>
-                            <p className={styles.projectsRowContentDescription}>Portfolio is just an example. A future project will be listed here.</p>
+                            <p className={styles.projectsRowContentDescription}>Portfolio is the page that you are looking at right now.</p>
                             <div className={styles.skills}>
                                 <div className={styles.skill}>HTML</div>
                                 <div className={styles.skill}>CSS</div>
@@ -25,7 +26,30 @@ export default function Projects(){
                                 <div className={styles.skill}>GIT</div>
                                 <div className={styles.skill}>Github</div>
                             </div>
-                            <a className={styles.projectsButton} href="https://samuelcondack.netlify.app/" target='_blank'>Live Link</a>
+                            <div className={styles.buttons}>
+                                <a className={styles.projectsButton} href="https://samuelcondack.netlify.app/" target='_blank'>Live Link</a>
+                                <a className={styles.gitHubButton} href="https://github.com/SamuelCondack/Portfolio" target='_blank'>GitHub</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={styles.projectsRow}>
+                        <div className={styles.rowImgContainer}>
+                            <img className={styles.projectsRowImg} src={Project2} alt="Project image" loading='lazy'/>
+                        </div>
+                        <div className={styles.projectsRowContent}>
+                            <h3 className={styles.projectsRowContentTitle}>LogIn & SignUp</h3>
+                            <p className={styles.projectsRowContentDescription}>A page with the system to create and log into an account. It has all the password and e-mail verification done with REGEX</p>
+                            <div className={styles.skills}>
+                                <div className={styles.skill}>HTML</div>
+                                <div className={styles.skill}>CSS</div>
+                                <div className={styles.skill}>JavaScript</div>
+                                <div className={styles.skill}>GIT</div>
+                                <div className={styles.skill}>Github</div>
+                            </div>
+                            <div className={styles.buttons}>
+                                <a className={styles.projectsButton} href="https://sam-login-signup.netlify.app/" target='_blank'>Live Link</a>
+                                <a className={styles.gitHubButton} href="https://github.com/SamuelCondack/Login-Signup" target='_blank'>GitHub</a>
+                            </div>
                         </div>
                     </div>
                 </div>
